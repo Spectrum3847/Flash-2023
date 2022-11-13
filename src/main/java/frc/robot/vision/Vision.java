970cce556ec336dfbd047c97ad3c2f2123fb788b
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2022-11-12 19:33:04 -0600

    created files

---
+++ src/main/java/frc/robot/vision/Vision.java
@@ -0,0 +1,9 @@
+package frc.robot.vision;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Vision extends SubsystemBase {
+    public VisionConfig config = new VisionConfig();
+
+    
+}
---
+++ src/main/java/frc/robot/vision/VisionConfig.java
@@ -0,0 +1,10 @@
+package frc.robot.vision;
+
+public final class VisionConfig {
+    
+
+    public VisionConfig() {
+        
+    }
+    
+}
