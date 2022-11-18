793e1ee14b298f8f4a1df6295de03c94c9096695
Allen Gregory <allengregoryiv@gmail.com>
2022-11-18 04:50:19 -0600

    clean up

--- src/main/java/frc/robot/leds/commands/LEDCommands.java
+++ src/main/java/frc/robot/leds/commands/LEDCommands.java
@@ -2,9 +2,5 @@ package frc.robot.leds.commands;
 
 /** All of the commands to schedule LEDs */
 public class LEDCommands {
-
-    public static void setupDefaultCommand() {
-        // Robot.leds.scheduler.setDefaultAnimation(
-        //        "Default LEDs", new BlinkLEDCommand(Color.kPurple));
-    }
+    public static void setupDefaultCommand() {}
 }
