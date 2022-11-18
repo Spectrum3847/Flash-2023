70cdc10a4d111c25019119f10aaf9429e16e8911
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2022-11-18 15:51:38 -0600

    Create BuildConstants.java

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "Flash-2023";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 48;
+    public static final String GIT_SHA = "3ca77c33e1f8117182b02bac2fd424e02291f3a8";
+    public static final String GIT_DATE = "2022-11-15 03:35:27 CST";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2022-11-15 17:35:38 CST";
+    public static final long BUILD_UNIX_TIME = 1668555338782L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants() {}
+}
