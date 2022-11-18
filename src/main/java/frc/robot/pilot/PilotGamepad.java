a55a5d9a06fdf8aa26a2edbc45c4279d085a281e
Allen Gregory <allengregoryiv@gmail.com>
2022-11-18 03:03:06 -0600

    update Pilot to the new axisbutton changes

--- src/main/java/frc/robot/pilot/PilotGamepad.java
+++ src/main/java/frc/robot/pilot/PilotGamepad.java
@@ -1,28 +1,27 @@
 package frc.robot.pilot;
 
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.SpectrumLib.gamepads.AxisButton;
+import frc.SpectrumLib.gamepads.AxisButton.ThresholdType;
 import frc.SpectrumLib.gamepads.Gamepad;
-import frc.SpectrumLib.gamepads.mapping.ExpCurve;
+import frc.SpectrumLib.gamepads.XboxGamepad.XboxAxis;
 import frc.robot.pilot.commands.PilotCommands;
 import frc.robot.swerve.commands.LockSwerve;
+import frc.robot.swerve.commands.SwerveCommands;
 
 /** Used to add buttons to the pilot gamepad and configure the joysticks */
 public class PilotGamepad extends Gamepad {
-    private double lastDriveAngle = 0;
-    private double lastRightStickAngle = 0;
-    public static ExpCurve steeringCurve =
-            new ExpCurve(
-                    PilotConfig.steeringExp,
-                    0,
-                    PilotConfig.steeringScaler,
-                    PilotConfig.steeringDeadband);
 
     public PilotGamepad() {
         super("PILOT", PilotConfig.port);
         gamepad.leftStick.setDeadband(PilotConfig.throttleDeadband);
-        gamepad.leftStick.configXCurve(PilotConfig.throttleExp, PilotConfig.xScaler);
-        gamepad.leftStick.configYCurve(PilotConfig.throttleExp, PilotConfig.yScaler);
+        gamepad.leftStick.configCurves(PilotConfig.throttleExp, PilotConfig.throttleScaler);
+        gamepad.leftStick.setXinvert(PilotConfig.xInvert);
+        gamepad.leftStick.setYinvert(PilotConfig.yInvert);
+
+        gamepad.triggers.setTwistDeadband(PilotConfig.steeringDeadband);
+        gamepad.triggers.configTwistCurve(PilotConfig.steeringExp, PilotConfig.steeringScaler);
+        gamepad.triggers.setTwistInvert(PilotConfig.steeringInvert);
     }
 
     public void setupTeleopButtons() {
@@ -31,7 +30,20 @@ public class PilotGamepad extends Gamepad {
         gamepad.bButton.whileTrue(PilotCommands.fpvPilotSwerve());
         gamepad.xButton.whileTrue(new LockSwerve());
         gamepad.yButton.whileTrue(PilotCommands.snakeDrive());
+
+        // Right Stick points the robot in that direction
+        Trigger rightX =
+                new AxisButton(gamepad, XboxAxis.RIGHT_X, 0.5, ThresholdType.DEADBAND).trigger();
+        Trigger rightY =
+                new AxisButton(gamepad, XboxAxis.RIGHT_Y, 0.5, ThresholdType.DEADBAND).trigger();
+        rightX.or(rightY).whileTrue(PilotCommands.stickSteer());
+
         gamepad.rightBumper.whileTrue(PilotCommands.stickSteer());
+
+        gamepad.Dpad.Up.whileTrue(SwerveCommands.setHeadingDeg(0));
+        gamepad.Dpad.Left.whileTrue(SwerveCommands.setHeadingDeg(90));
+        gamepad.Dpad.Down.whileTrue(SwerveCommands.setHeadingDeg(180));
+        gamepad.Dpad.Right.whileTrue(SwerveCommands.setHeadingDeg(270));
     }
 
     public void setupDisabledButtons() {}
@@ -40,45 +52,28 @@ public class PilotGamepad extends Gamepad {
 
     public double getDriveX() {
         double x = gamepad.leftStick.getX();
-        // Robot.log.logger.recordOutput("Pilot/X", x);
         return x;
     }
 
     public double getDriveY() {
         double y = gamepad.leftStick.getY();
-        // Robot.log.logger.recordOutput("Pilot/Y", y);
         return y;
     }
 
     // Positive is counter-clockwise, left Trigger is positive
     public double getDriveR() {
-        double r = steeringCurve.calculateMappedVal(gamepad.triggers.getTwist());
-
+        double r = gamepad.triggers.getTwist();
         return r;
     }
 
+    // Return the angle created by the left stick in radians, 0 is up, 90 is left
     public Double getDriveAngle() {
-        double x = getDriveX() * -1; // Xis flipped to get direction correctly
-        double y = getDriveY();
-        if (x != 0 || y != 0) {
-            Translation2d translation = new Translation2d(x, y);
-            // Minus 90 to switch Y axis to be 0 degrees
-            double angle = translation.getAngle().getDegrees() - 90;
-            lastDriveAngle = angle;
-        }
-        return Units.degreesToRadians(lastDriveAngle);
+        return gamepad.leftStick.getDirectionRadians();
     }
 
+    // Return the angle created by the right stick in radians, 0 is up, 90 is left
     public double getRightStickAngle() {
-        double x = gamepad.rightStick.getX();
-        double y = gamepad.rightStick.getY() * -1;
-
-        if (Math.abs(x) <= 0.5 || Math.abs(y) <= 0.5) {
-            Translation2d translation = new Translation2d(x, y);
-            double angle = translation.getAngle().getDegrees() - 90;
-            lastRightStickAngle = angle;
-        }
-        return Units.degreesToRadians(lastRightStickAngle);
+        return gamepad.rightStick.getDirectionRadians();
     }
 
     public void rumble(double intensity) {
