1995ea3deba6b86dfb12bc383712b4054bf0524e
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2022-11-20 15:42:54 -0600

    better command

--- src/main/java/frc/robot/vision/VisionCommands.java
+++ src/main/java/frc/robot/vision/VisionCommands.java
@@ -2,22 +2,18 @@ package frc.robot.vision;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import frc.robot.Robot;
 import frc.robot.RobotTelemetry;
 
 public class VisionCommands {
 
     public static Command printYawInfo() {
-        return new InstantCommand(
-                () -> {
-                    RobotTelemetry.print(
-                            "Yaw (D): "
-                                    + Robot.vision.getYaw()
-                                    + "|| gyro (D): "
-                                    + Robot.swerve.getHeading().getDegrees()
-                                    + " || Aiming at: "
-                                    + (Robot.vision.getYaw() + Robot.swerve.getHeading().getDegrees()));
-                },
-                Robot.vision);
+        return new PrintCommand("Yaw (D): "
+        + Robot.vision.getYaw()
+        + "|| gyro (D): "
+        + Robot.swerve.getHeading().getDegrees()
+        + " || Aiming at: "
+        + (Robot.vision.getYaw() + Robot.swerve.getHeading().getDegrees()));
     }
 }
