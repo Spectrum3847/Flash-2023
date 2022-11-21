809035660b4f92b13405b7403acea8808a40e687
Daniel-J101 <jdaniel24@mail.strakejesuit.org>
2022-11-21 12:21:01 -0600

    started reorganizing
    unfinished and has errors

--- src/main/java/frc/robot/vision/VisionConfig.java
+++ src/main/java/frc/robot/vision/VisionConfig.java
@@ -13,23 +13,26 @@ import org.photonvision.PhotonCamera;
 
 @SuppressWarnings("unused")
 public final class VisionConfig {
-    public final String CAMERA_NAME = "gloworm";
-    // robot coordinate plane -- away is +x, left is +y, up is +z
-    public final double CAMERA_TO_ROBOT_X_METERS = Units.inchesToMeters(5.841);
-    public final double CAMERA_TO_ROBOT_Y_METERS = Units.inchesToMeters(-0.5);
-    public final double CAMERA_TO_ROBOT_Z_METERS =
+    public static Map<Integer, Pose3d> tagMap;
+    public static Pair<PhotonCamera, Transform3d> cameraPair;
+    public static ArrayList<Pair<PhotonCamera, Transform3d>> cameras;
+
+    /* April Tag setup */   
+    public static final AprilTag tag0 =
+            new AprilTag(0, new Pose3d(1.515, 7.286, 1.07, new Rotation3d(0, 0, -Math.PI / 2)));    
+    
+
+    public static final String CAMERA_NAME = "gloworm";
+
+    /* Robot coordinate plane || away is +x, left is +y, up is +z */
+    public static final double CAMERA_TO_ROBOT_X_METERS = Units.inchesToMeters(5.841);
+    public static final double CAMERA_TO_ROBOT_Y_METERS = Units.inchesToMeters(-0.5);
+    public static final double CAMERA_TO_ROBOT_Z_METERS =
             Units.inchesToMeters(16.178); // height from camera to robot center
+    public static final double CAMERA_PITCH_RADIANS =
+            Units.degreesToRadians(-26.138); // angle from looking straight forward -direction is looking up
 
-    public final double CAMERA_PITCH_RADIANS =
-            Units.degreesToRadians(-26.138); // angle from looking straight forward - is looking up
 
-    public AprilTag tag0 =
-            new AprilTag(0, new Pose3d(1.515, 7.286, 1.07, new Rotation3d(0, 0, -Math.PI / 2)));
-    public Map<Integer, Pose3d> tagMap;
-    public Pair<PhotonCamera, Transform3d> cameraPair;
-    public ArrayList<Pair<PhotonCamera, Transform3d>> cameras;
-
-    // testing
 
     public VisionConfig() {
         cameras = new ArrayList<Pair<PhotonCamera, Transform3d>>();
