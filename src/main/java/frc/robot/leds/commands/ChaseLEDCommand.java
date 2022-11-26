66a84bc529619959daaf8c302706b29a29c91948
rebeccabibye <76578313+rebeccabibye@users.noreply.github.com>
2022-11-26 15:26:16 -0600

    Chase LED working with a color gradient (kind of)
    It has five different pixels one after another, with the brightest in the middle and the color fading out at the sides

--- src/main/java/frc/robot/leds/commands/ChaseLEDCommand.java
+++ src/main/java/frc/robot/leds/commands/ChaseLEDCommand.java
@@ -13,7 +13,7 @@ public class ChaseLEDCommand extends LEDCommandBase {
 
     public ChaseLEDCommand() {
         ledSubsystem = Robot.leds;
-        this.waitTime = 100;
+        this.waitTime = 1;
         this.startTime = System.currentTimeMillis();
         onLEDIndex = 0;
     }
@@ -39,6 +39,22 @@ public class ChaseLEDCommand extends LEDCommandBase {
             for (int i = 0; i < ledSubsystem.getBufferLength(); i++) {
                 if (i == onLEDIndex) {
                     ledSubsystem.setRGB(i, 255, 255, 255);
+                    continue;
+                }
+                if (i == onLEDIndex + 1) {
+                    ledSubsystem.setRGB(i, 150, 150, 150);
+                    continue;
+                }
+                if (i == onLEDIndex - 1) {
+                    ledSubsystem.setRGB(i, 150, 150, 150);
+                    continue;
+                }
+                if (i == onLEDIndex + 2) {
+                    ledSubsystem.setRGB(i, 75, 75, 75);
+                    continue;
+                }
+                if (i == onLEDIndex - 2) {
+                    ledSubsystem.setRGB(i, 75, 75, 75);
                     continue;
                 }
                 ledSubsystem.setRGB(i, 0, 0, 0);
