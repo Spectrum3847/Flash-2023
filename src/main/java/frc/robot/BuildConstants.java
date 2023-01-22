204a8d61d363839a07d4a8ca1d2c4b4b7c72e6e7
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2023-01-21 20:12:30 -0600

    updated for Alpha 2023

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Flash-2023";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 152;
-    public static final String GIT_SHA = "55d50b3873683e8a520e950974a6a9b7a48ffb6d";
-    public static final String GIT_DATE = "2023-01-19 16:22:03 CST";
-    public static final String GIT_BRANCH = "auto";
-    public static final String BUILD_DATE = "2023-01-20 16:24:03 CST";
-    public static final long BUILD_UNIX_TIME = 1674253443115L;
+    public static final int GIT_REVISION = 153;
+    public static final String GIT_SHA = "93b1895cf4026671fb63de47f014761685a347b4";
+    public static final String GIT_DATE = "2023-01-20 16:49:01 CST";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2023-01-21 19:48:46 CST";
+    public static final long BUILD_UNIX_TIME = 1674352126797L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
