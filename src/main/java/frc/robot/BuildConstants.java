b847ed9715d9ae2345695ad8b19165dfe8d396dc
Evan Pendleton <94720437+EDPendleton24@users.noreply.github.com>
2023-01-25 17:17:48 -0600

    MAC address setup works now

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Flash-2023";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 174;
-    public static final String GIT_SHA = "74ea46d44df708b6885b9be82411ecd16faf42af";
-    public static final String GIT_DATE = "2023-01-25 15:42:11 CST";
+    public static final int GIT_REVISION = 175;
+    public static final String GIT_SHA = "a8fc86c907fb4a85a5d04906c9a2176ab030e790";
+    public static final String GIT_DATE = "2023-01-25 17:07:55 CST";
     public static final String GIT_BRANCH = "auto";
-    public static final String BUILD_DATE = "2023-01-25 17:04:38 CST";
-    public static final long BUILD_UNIX_TIME = 1674687878244L;
+    public static final String BUILD_DATE = "2023-01-25 17:09:07 CST";
+    public static final long BUILD_UNIX_TIME = 1674688147718L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
